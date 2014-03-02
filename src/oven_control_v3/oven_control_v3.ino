e556a0d3db6bbce8c2d9a3514f154cff1b18d5e0
Jenner Hanni <jeh.wicker@gmail.com>
2014-03-02 13:14:51 -0800

    Updated sketch to reflect new pinouts. It compiles and runs. Not tested with oven board attached, only LCD.

--- src/oven_control_v3/oven_control_v3.ino
+++ src/oven_control_v3/oven_control_v3.ino
@@ -1,31 +1,35 @@
-/*************************************************** 
+  /*************************************************** 
   On/ Off control 1 thermocouples 
  ****************************************************/
 
 #include "Adafruit_MAX31855.h"
 
+#define OFF 0
+#define ON  1
+
 // Pins connection
-int thermoDO      = 3;
-int thermoCLK     = 4;
-int thermoCS1     = 5;
-int thermoCS2     = 6;
-int thermoCS3     = 7;
-int thermoCS4     = 8;
+int swReset       = 0;
+int thermoCS1     = 1;
+int thermoCS2     = 2;
+int thermoCS3     = 3;
+int sdCS          = 4;
+int heater1       = 5;
+int heater2       = 6;
+int thermoCS4     = 7;
+int lcdDC         = 8;
 int thermoCS5     = 9;
-int thermoCS6     = 10;
-int heaterPins1   = 11;
-int heaterPins2   = 12;
+int lcdCS         = 10;
+int spiMOSI       = 11;
+int spiMISO       = 12;
+int spiCLK        = 13;
 int swStart       = A0;
-int swMode1       = A1;
-int swMode2       = A2;
-int swMode3       = A3;
+int fan           = A1;
+int vent          = A2;
+int joyRead       = A3;
 
 // Indicate status variables: 0 off; 1: on
-int mode1      =  0;
-int mode2      =  0;
-int mode3      =  0;
-int start      =  0;
-int stopnow    =  0;
+int start      =  OFF;
+int stopnow    =  ON;
 
 // constant
 double tempSet         =  350;        // farenheit
@@ -43,8 +47,6 @@ double sensor2  =  -4;
 double sensor3  =  0;
 double sensor4  =  0;
 double sensor5  =  0;
-double sensor6  =  0;
-
 
 // variables
 double tempErr;
@@ -53,7 +55,6 @@ double tempSensor2;
 double tempSensor3;
 double tempSensor4;
 double tempSensor5;
-double tempSensor6;
 double tempAverage;
 double tempInitial;         // room temp
 
@@ -65,50 +66,50 @@ double startTime;
 double currTime;
 
 // Thermocouples
-Adafruit_MAX31855 thermocouple1(thermoCLK, thermoCS1, thermoDO);
-Adafruit_MAX31855 thermocouple2(thermoCLK, thermoCS2, thermoDO);
-Adafruit_MAX31855 thermocouple3(thermoCLK, thermoCS3, thermoDO);
-Adafruit_MAX31855 thermocouple4(thermoCLK, thermoCS4, thermoDO);
-Adafruit_MAX31855 thermocouple5(thermoCLK, thermoCS5, thermoDO);
-Adafruit_MAX31855 thermocouple6(thermoCLK, thermoCS6, thermoDO);
-
+Adafruit_MAX31855 thermocouple1(spiCLK, thermoCS1, spiMISO);
+Adafruit_MAX31855 thermocouple2(spiCLK, thermoCS2, spiMISO);
+Adafruit_MAX31855 thermocouple3(spiCLK, thermoCS3, spiMISO);
+Adafruit_MAX31855 thermocouple4(spiCLK, thermoCS4, spiMISO);
+Adafruit_MAX31855 thermocouple5(spiCLK, thermoCS5, spiMISO);
 
 void all_heaters_off()
 {
-  digitalWrite(heaterPins1, LOW);  // turn off the heaters
+  digitalWrite(heater1, LOW);  // turn off the heaters
   delay(100);
-  digitalWrite(heaterPins2, LOW);  // turn off the heaters
+  digitalWrite(heater2, LOW);  // turn off the heaters
   Serial.println("  OFF");  
 }
 
 void one_heater_off()
 {
-  digitalWrite(heaterPins1, LOW);  // turn off the heaters
+  digitalWrite(heater1, LOW);  // turn off the heaters
   delay(100);
   Serial.println("  1 OFF");  
 }
 
 void all_heaters_on()
 {
-  digitalWrite(heaterPins1, HIGH);  // turn on the heaters
+  digitalWrite(heater1, HIGH);  // turn on the heaters
   delay(100);
-  digitalWrite(heaterPins2, HIGH);  // turn on the heaters
+  digitalWrite(heater2, HIGH);  // turn on the heaters
   delay(100);
   Serial.println("  ON");  
 }
  
 void one_heater_on()
 {
-  digitalWrite(heaterPins1, HIGH);  // turn on the heaters
+  digitalWrite(heater1, HIGH);  // turn on the heaters
   delay(100);
   Serial.println("  1 ON");  
 } 
   
 void setup() {
   Serial.begin(9600);
-  pinMode(heaterPins1, OUTPUT);
-  pinMode(heaterPins2, OUTPUT);
-  // wait for MAX chip to stabilize
+  pinMode(heater1, OUTPUT);
+  pinMode(heater2, OUTPUT);
+  pinMode(fan, OUTPUT);
+  pinMode(vent, OUTPUT);
+  // wait for MAX chips to stabilize
   delay(1000);
   all_heaters_off();
   Serial.println("MAX31855 test");
@@ -127,7 +128,7 @@ int scan_button(int swButton)
 void tempPrint()
 {
   Serial.print("Current temperature F = ");
- // Serial.println(tempSensor1,"\t", tempSensor2,"\t",tempSensor3,"\t",tempSensor4,"\t",tempSensor5,"\t",tempSensor6);
+ // Serial.println(tempSensor1,"\t", tempSensor2,"\t",tempSensor3,"\t",tempSensor4,"\t",tempSensor5);
   Serial.print(tempSensor1);
   Serial.print(" ");
   Serial.print(tempSensor2);
@@ -136,9 +137,7 @@ void tempPrint()
   Serial.print(" ");
   Serial.print(tempSensor4);
   Serial.print(" ");
-  Serial.print(tempSensor5);
-  Serial.print(" ");
-  Serial.println(tempSensor6);  
+  Serial.print(tempSensor5); 
   Serial.print("Average temperature F = ");
   Serial.println(tempAverage);
 }
@@ -147,8 +146,6 @@ void tempPrint()
 void sensors_reading()
 {
     // scanning all of the sensors and switches
-  mode1 = scan_button(swMode1);
-  mode2 = scan_button(swMode2);
   start = scan_button(swStart);
   float tempSum=0;
   int count=0;
@@ -179,20 +176,14 @@ void sensors_reading()
       count=count+1;
       tempSum=tempSum+tempSensor4;
     }*/
-  //delay(10);
+  delay(10);
   tempSensor5 = thermocouple5.readFarenheit()+sensor5;
-    /*if (tempSensor5>32) // eliminate the SPI disconection 
+    if (tempSensor5>32) // eliminate the SPI disconection 
     {
       count=count+1;
       tempSum=tempSum+tempSensor5;
-    }*/
-  //delay(10);
-  tempSensor6 = thermocouple6.readFarenheit()+sensor6;
-    /*if (tempSensor6>32) // eliminate the SPI disconection 
-    {
-      count=count+1;
-      tempSum=tempSum+tempSensor6;
-    }*/
+    }
+  delay(10);
   tempAverage = tempSum/count;
   tempPrint();
 }
@@ -238,5 +229,6 @@ void loop() {
     while(1);  // stay here until turn off the controller.
   }
   delay(timeDelay);
+  
 } 
 
